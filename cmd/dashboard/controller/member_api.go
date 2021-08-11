c58c3c906abb857766d3e1a519038bfb2a311466
佩佩 <admin@nange.cn>
2021-08-11 19:36:41 +0800

    Update.

--- cmd/dashboard/controller/member_api.go
+++ cmd/dashboard/controller/member_api.go
@@ -453,6 +453,7 @@ type settingForm struct {
 	IgnoredIPNotification      string
 	Oauth2Type                 string
 	GRPCHost                   string
+	GRPCPort                   uint
 	Cover                      uint8
 }
 
@@ -468,6 +469,7 @@ func (ma *memberAPI) updateSetting(c *gin.Context) {
 	dao.Conf.EnableIPChangeNotification = sf.EnableIPChangeNotification == "on"
 	dao.Conf.Cover = sf.Cover
 	dao.Conf.GRPCHost = sf.GRPCHost
+	dao.Conf.GRPCPort = sf.GRPCPort
 	dao.Conf.IgnoredIPNotification = sf.IgnoredIPNotification
 	dao.Conf.Site.Brand = sf.Title
 	dao.Conf.Site.Theme = sf.Theme
