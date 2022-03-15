75d1a158651519fe236eac6ee7ba6a7028e417d4
翠花 <admin@nange.cn>
2022-03-15 20:03:36 +0800

    Update.

--- cmd/agent/monitor/myip.go
+++ cmd/agent/monitor/myip.go
@@ -33,24 +33,22 @@ func UpdateIP() {
 	for {
 		ipv4 := fetchGeoIP(geoIPApiList, false)
 		ipv6 := fetchGeoIP(geoIPApiList, true)
+
 		if ipv4.IP == "" && ipv6.IP == "" {
-			cachedIP = fmt.Sprintf("IPs(未获取到 IP%s%s)", ipv4.IP, ipv6.IP)
+			cachedIP = fmt.Sprintf("IPs[未获取到 IP]")
 			time.Sleep(time.Second*30)
 			continue
 		} 
 		if ipv4.IP != "" && ipv6.IP == "" {
-			cachedIP = fmt.Sprintf("IPs(IPv4:%s%s)", ipv4.IP, ipv6.IP)
-		} 
-		if ipv4.IP == "" && ipv6.IP != "" {
-			cachedIP = fmt.Sprintf("IPs(IPv6:%s[%s])", ipv4.IP, ipv6.IP)
-		} 
-		if ipv4.IP != "" && ipv6.IP != "" {
-			cachedIP = fmt.Sprintf("IPs(IPv4:%s,IPv6:[%s])", ipv4.IP, ipv6.IP)
+			cachedIP = fmt.Sprintf("IPs[IPv4:%s]", ipv4.IP)
+		} else if ipv4.IP == "" && ipv6.IP != "" {
+			cachedIP = fmt.Sprintf("IPs[IPv6:[%s]]", ipv6.IP)
+		} else {
+			cachedIP = fmt.Sprintf("IPs[IPv4:%s,IPv6:[%s]]", ipv4.IP, ipv6.IP)
 		} 
 		if ipv4.CountryCode != "" {
 			cachedCountry = ipv4.CountryCode
-		} 
-		if ipv4.CountryCode == "" && ipv6.CountryCode != "" {
+		} else if ipv6.CountryCode != "" {
 			cachedCountry = ipv6.CountryCode
 		}
 		time.Sleep(time.Minute * 30)
